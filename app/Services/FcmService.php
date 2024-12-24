febb5a6a43c4903353ff9cb475b02bc640e470bb
Eman Yaghi <135706090+EmanYaghi@users.noreply.github.com>
2024-12-24 21:10:37 +0300

    Update FcmService.php

--- app/Services/FcmService.php
+++ app/Services/FcmService.php
@@ -1,31 +1,65 @@
 <?php
-
-namespace AppServices;
-
+namespace App\Services\Api;
+use App\Models\Notification as NotificationModel;
+use Illuminate\Support\Facades\Log;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\Messaging\CloudMessage;
-use Kreait\Firebase\Messaging\Notification;
-
 class FcmService
 {
-    protected $messaging;
-
-    public function __construct()
+    public function index()
     {
-        $firebase = (new Factory)
-            ->withServiceAccount(base_path(env('FIREBASE_CREDENTIALS')));
-
-        $this->messaging = $firebase->createMessaging();
+        return auth()->user()->notifications;
     }
-
-    public function sendNotification($deviceToken, $title, $body, array $data = [])
+    public function send($user, $title, $message, $type = 'basic')
     {
-        $notification = Notification::create($title, $body);
+        // Path to the service account key JSON file
+        $serviceAccountPath = storage_path('app/firebase_credentials.json');
 
-        $message = CloudMessage::withTarget('token', $deviceToken)
+        // Initialize the Firebase Factory with the service account
+        $factory = (new Factory)->withServiceAccount($serviceAccountPath);
+        
+        $messaging = $factory->createMessaging();
+
+        // Prepare the notification array
+        $notification = [
+            'title' => $title,
+            'body' => $message,
+            'sound' => 'default',
+        ];
+        // Additional data payload
+        $data = [
+            'type' => $type,
+            'id' => $user['id'],
+            'message' => $message,
+        ];
+
+        // Create the CloudMessage instance
+        $cloudMessage = CloudMessage::withTarget('token', $user['fcm_token'])
             ->withNotification($notification)
             ->withData($data);
 
-        return $this->messaging->send($message);
+        try {
+            // Send the notification
+            $messaging->send($cloudMessage);
+
+            // Save the notification to the database
+            NotificationModel::query()->create([
+                'type' => 'App\Notifications\UserFollow',
+                'notifiable_type' => 'App\Models\User',
+                'notifiable_id' => $user['id'],
+                'data' => json_encode([
+                    'user' => $user['first_name'] . ' ' . $user['last_name'],
+                    'message' => $message,
+                    'title' => $title,
+                ]), // The data of the notification
+            ]);
+            return 1;
+        } catch (\Kreait\Firebase\Exception\MessagingException $e) {
+            Log::error($e->getMessage());
+            return 0;
+        } catch (\Kreait\Firebase\Exception\FirebaseException $e) {
+            Log::error($e->getMessage());
+            return 0;
+        }
     }
 }

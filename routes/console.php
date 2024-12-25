2f10eb1952d845064c8bdda0bcfd8ac1121d794f
kareem-sh <130371958+kareem-sh@users.noreply.github.com>
2024-12-25 23:06:23 +0100

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Order;
+use AppServices\FcmService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,23 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+Schedule::call(function () {
+    $orders=Order::all();
+    foreach($orders as $order)
+    {
+        if($order->order_date<=now()->subMinutes(1)&&$order->order_status=="preparing")
+        {
+           $order->update(['order_status'=>"on the way"]);
+            $service=new FcmService();
+           $service->sendNotification($order->user->fcm_token,"Hello ".$order->user->full_name,"the order with product ".$order->product->name." on the way",[$order->id]);
+        }
+        else if($order->order_date<=now()->subMinutes(2)&&$order->order_status=="on the way")
+        {
+            $order->update(['order_status'=>"delivered"]);
+             $service=new FcmService();
+            $service->sendNotification($order->user->fcm_token,"Hello ".$order->user->full_name,"the order with product ".$order->product->name." delivered",[$order->id]);
+        }
+    }
+})->everyMinute();
 })->purpose('Display an inspiring quote');
+
